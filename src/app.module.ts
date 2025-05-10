72d1bd1e37ad4fbd5dfb551f321bfb801624e566
septi <septirizky79@gmail.com>
2025-05-10 23:11:27 +0700

    inect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config';
+import { User } from './users/user.model';
+import { TouristsModule } from './tourists/tourists.module';
+import { TripsModule } from './trips/trips.module';
+import { TripReviewsModule } from './trip-reviews/trip-reviews.module';
+import { Tourist } from './tourists/tourist.model';
+import { Trip } from './trips/trip.model';
+import { TripReview } from './trip-reviews/trip_review.model';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,7 +24,12 @@ import { ConfigModule } from '@nestjs/config';
       database: process.env.DB_NAME,
       autoLoadModels: true,
       synchronize: true,
+      models: [User, Tourist, Trip, TripReview],
     }),
+    TouristsModule,
+    TripsModule,
+    TripReviewsModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'assets'));
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server running on http://localhost:${port}`);
 }
 bootstrap();
